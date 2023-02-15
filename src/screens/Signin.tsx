2f6ec6711599f4b1b13b78a439f789cdc5d0adc5
João Paulo Mesquita <jp-mesquita@live.com>
2023-02-15 13:45:25 -0300

    feat: add input  component to signin page

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,11 +1,12 @@
-import { VStack, Image, Text, Center, Heading} from "native-base"
+import { VStack, Image, Text, Center, Heading, ScrollView} from "native-base"
 
 import BackgroundImg from '@assets/background.png'
 import  LogoSvg from  '@assets/logo.svg'
+import { Input } from "@components/Input"
 
 export function SignIn() {
   return(
-    <VStack flex={1} bg='gray.700'>
+    <VStack flex={1} bg='gray.700' px={10}>
       <Image 
         source={BackgroundImg}
         alt="Pessoas treinando"
@@ -16,16 +17,35 @@ export function SignIn() {
       <Center my={24}>
         <LogoSvg />
 
-        <Text color='gray.100'  fontSize='sm' mb={6} fontFamily='heading'>
+        <Text color='gray.100'  fontSize='sm' >
           Treine sua mente  e o seu corpo!
         </Text>
       </Center>
 
-      <Center>
-        <Heading color='gray.100'>
-          Acesse sua conta
-        </Heading>
-      </Center>
+      <ScrollView
+        contentContainerStyle={{marginBottom: 200}}
+        showsVerticalScrollIndicator={false}
+      >
+
+        <Center>
+          <Heading color='gray.100' fontSize='xl' mb={6} fontFamily='heading'>
+            Acesse sua conta
+          </Heading>
+
+          <Input 
+            placeholder="E-mail"
+            keyboardType="email-address"  
+            autoCapitalize="none"
+            />
+
+          <Input 
+            placeholder="Senha"
+            secureTextEntry
+            
+            />
+
+        </Center>
+      </ScrollView>
 
     </VStack>
   )
