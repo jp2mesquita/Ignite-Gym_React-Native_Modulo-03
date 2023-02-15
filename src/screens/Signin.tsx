6fe6cd89b3342d97926607a4dffafbbb01fdc6eb
João Paulo Mesquita <jp-mesquita@live.com>
2023-02-15 11:24:48 -0300

    feat: add Heading component

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,6 +1,7 @@
-import { VStack, Image } from "native-base"
+import { VStack, Image, Text, Center, Heading} from "native-base"
 
 import BackgroundImg from '@assets/background.png'
+import  LogoSvg from  '@assets/logo.svg'
 
 export function SignIn() {
   return(
@@ -11,6 +12,19 @@ export function SignIn() {
         resizeMode="contain"
         position='absolute'
       />
+
+      <Center my={24}>
+        <LogoSvg />
+
+        <Text color='gray.100'  fontSize='sm' mb={6} fontFamily='heading'>
+          Treine sua mente  e o seu corpo!
+        </Text>
+      </Center>
+
+      <Heading color='gray.100'>
+        Acesse sua conta
+      </Heading>
+
     </VStack>
   )
 }
