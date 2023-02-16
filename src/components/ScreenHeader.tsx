d397385dea0ee801b8b027c6432fe5479f5b9461
João Paulo Mesquita <jp-mesquita@live.com>
2023-02-16 15:33:45 -0300

    feat: create a dinamic screen header

---
+++ src/components/ScreenHeader.tsx
@@ -0,0 +1,15 @@
+import { Heading, Center } from "native-base";
+
+type ScreenHeaderProps = {
+  title: string
+}
+
+export function ScreenHeader({ title }: ScreenHeaderProps) {
+  return(
+    <Center pt={16} pb={6} bg='gray.600'>
+      <Heading color='gray.100' fontSize='xl'>
+        {title}
+      </Heading>
+    </Center>
+  )
+}
