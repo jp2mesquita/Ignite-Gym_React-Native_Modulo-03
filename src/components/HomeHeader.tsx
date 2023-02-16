aaa0144edbc1ae6516d8cca13a80f566c49f596d
João Paulo Mesquita <jp-mesquita@live.com>
2023-02-16 11:31:29 -0300

    feat: add logout icon

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,20 +1,22 @@
-import { Heading, HStack, Text, VStack } from "native-base";
+import { TouchableOpacity } from "react-native";
+import { Heading, HStack, Text, VStack, Icon, Row } from "native-base";
+import { MaterialIcons } from '@expo/vector-icons'
 
 import { UserPhoto } from "./UserPhoto";
 
 
-
 export function HomeHeader(){
   return (
-    <HStack bg='gray.600' pt={16} pb={5} px={8} alignItems='center'>
+    <HStack bg='gray.600' pt={16} pb={5} px={8} alignItems='center' >
+
       <UserPhoto 
         source={{ uri: 'http://github.com/jp2mesquita.png'}}
         alt="Imagem de perfil do usuário"
         size={16}
         mr={4}
-      />
+        />
 
-      <VStack>
+      <VStack flex={1}>
         <Text color='gray.100' fontSize='md'> 
           Olá, 
         </Text>
@@ -23,6 +25,17 @@ export function HomeHeader(){
           Jaum
         </Heading>
       </VStack>
+   
+
+      <TouchableOpacity>
+        <Icon 
+          as={MaterialIcons} 
+          name='logout' 
+          color='gray.200' 
+          size={7}
+        />
+      </TouchableOpacity>
+     
     </HStack>
   )
 }
