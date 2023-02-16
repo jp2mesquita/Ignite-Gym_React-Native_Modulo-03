aa9972e06646ad1d090c2a2f965a4e264405fe80
João Paulo Mesquita <jp-mesquita@live.com>
2023-02-16 10:41:45 -0300

    feat: create HomeHeader component

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,28 @@
+import { Heading, HStack, Text, VStack } from "native-base";
+
+import { UserPhoto } from "./UserPhoto";
+
+
+
+export function HomeHeader(){
+  return (
+    <HStack bg='gray.600' pt={16} pb={5} px={8} alignItems='center'>
+      <UserPhoto 
+        source={{ uri: 'http://github.com/jp2mesquita.png'}}
+        alt="Imagem de perfil do usuário"
+        size={16}
+        mr={4}
+      />
+
+      <VStack>
+        <Text color='gray.100' fontSize='md'> 
+          Olá, 
+        </Text>
+
+        <Heading color='gray.100' fontSize='md'>
+          Jaum
+        </Heading>
+      </VStack>
+    </HStack>
+  )
+}
