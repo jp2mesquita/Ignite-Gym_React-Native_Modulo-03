0b772e4b78bde40f4f6684f7d66c7f5198ccc0c4
João Paulo Mesquita <jp-mesquita@live.com>
2023-02-16 14:56:56 -0300

    feat: create exercise cards list on home page

---
+++ src/screens/ExerciseCard.tsx
@@ -0,0 +1,50 @@
+import { Heading, HStack, Text, VStack, Icon, useTheme, Image } from "native-base";
+import { TouchableOpacity, TouchableOpacityProps } from "react-native";
+import { Entypo } from '@expo/vector-icons'
+
+type ExerciseCardProps = TouchableOpacityProps & {
+  title: string
+  description: string
+  imageUri: string
+}
+
+export function ExerciseCard({description, imageUri, title, ...rest}: ExerciseCardProps){
+  const { colors } = useTheme()
+
+  return(
+    <TouchableOpacity
+      {...rest}
+    >
+      <HStack bg='gray.500' p={2} alignItems='center' pr={4} rounded='md' mb={3}>
+        <Image  
+          source={{ uri: imageUri}}
+          alt='Exercício de barra fixa'
+          w={16}
+          h={16}
+          rounded='md'
+          mr={4}
+          resizeMode='center'
+        />
+
+        <VStack flex={1}>
+          <Heading color='white' fontFamily='heading' fontSize='lg'>
+            {title}
+          </Heading>
+
+          <Text color='gray.200' fontFamily='body' fontSize='sm' mt={1} numberOfLines={2}>
+            3 séries x 12 repetições
+        </Text>
+        </VStack>
+
+        <TouchableOpacity>
+          <Icon 
+            as={Entypo}
+            name='chevron-thin-right'
+            color='gray.300' 
+            size={7}
+          />
+        </TouchableOpacity>
+      </HStack>
+    </TouchableOpacity>
+  )
+}
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,13 +2,16 @@ import { useState } from "react";
 
 import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
-import { HStack, VStack, FlatList } from "native-base";
+import { HStack, VStack, FlatList, Heading, Text } from "native-base";
+import { ExerciseCard } from "./ExerciseCard";
 
 
 
 export function Home(){
   const [groups, setGroups]= useState(['Costas', "Ombros", "Bíceps", 'Tríceps' ])
-  const [ groupSelected, setGroupSelected ] = useState('costas')
+  const [exercises, setexErcises]= useState(['Puxada frontal', "Remada  curvada", "Remada unilateral", 'Levantamento terra' ])
+
+  const [ groupSelected, setGroupSelected ] = useState('Costas')
 
   function handleSelectGroup(group: string){
     setGroupSelected(group)
@@ -24,17 +27,48 @@ export function Home(){
           return(
             <Group 
               name={item} 
-              isActive={groupSelected === item}  
+              isActive={String(groupSelected).toUpperCase() === String(item).toUpperCase()}  
               onPress={() => handleSelectGroup(item)}
             />
           )
         }}
         horizontal
-        showsHorizontalScrollIndicator
+        showsHorizontalScrollIndicator={false}
         _contentContainerStyle={{ px: 8}}
         my={10}
         maxH={10}
       />
+
+      <VStack flex={1} px={8}>
+        <HStack justifyContent='space-between' mb={5}>
+          <Heading color='gray.200' fontSize='md'>
+            Exercícios
+          </Heading>
+
+          <Text color='gray.200' fontSize='sm'>
+            {exercises.length}
+          </Text>
+        </HStack>
+
+        <FlatList 
+          data={exercises}
+          keyExtractor={item => item}
+          renderItem={ ({ item }) => {
+            return(
+              <ExerciseCard 
+                title={item}
+                imageUri='https://images.unsplash.com/photo-1526506118085-60ce8714f8c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8dW5pbGF0ZXJhbCUyMHB1bGx1cHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60'
+                description={item}
+              />
+            )
+          }}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{
+            paddingBottom: 16
+          }}
+        />
+        
+      </VStack>
     </VStack>
   )
 }
