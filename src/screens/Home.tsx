f4aa047524fb8e1861c7e1f3b7d45a655df61dc7
João Paulo Mesquita <jp-mesquita@live.com>
2023-02-16 11:31:52 -0300

    feat: create groups selection list

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,40 @@
+import { useState } from "react";
+
+import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
-import { VStack } from "native-base";
+import { HStack, VStack, FlatList } from "native-base";
+
+
 
 export function Home(){
+  const [groups, setGroups]= useState(['Costas', "Ombros", "Bíceps", 'Tríceps' ])
+  const [ groupSelected, setGroupSelected ] = useState('costas')
+
+  function handleSelectGroup(group: string){
+    setGroupSelected(group)
+  }
   return(
     <VStack flex={1}>
       <HomeHeader />
+
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) =>{
+          return(
+            <Group 
+              name={item} 
+              isActive={groupSelected === item}  
+              onPress={() => handleSelectGroup(item)}
+            />
+          )
+        }}
+        horizontal
+        showsHorizontalScrollIndicator
+        _contentContainerStyle={{ px: 8}}
+        my={10}
+        maxH={10}
+      />
     </VStack>
   )
 }
