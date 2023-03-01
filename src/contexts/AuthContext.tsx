6666e5a4813ae82d6ca09ab6e1cec7351e2b9337
João Paulo Mesquita <jp-mesquita@live.com>
2023-03-01 13:42:34 -0300

    feat: add async storage to persist user and get user info when loading

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,10 +1,12 @@
 import { UserDTO } from "@dtos/UserDTO";
 import { api } from "@services/api";
+import { storageUserGet, storageUserSave } from "@storage/storageUser";
 
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 
 export type AuthContextDataProps = {
   user: UserDTO
+  isLoadingUserStorageData: boolean
   signIn: (email: string, password: string) => Promise<void>
 }
 
@@ -16,6 +18,7 @@ export const AuthContext = createContext<AuthContextDataProps>({} as AuthContext
 
 export function AuthContextProvider ({ children }: AuthContextProviderProps) {
   const [user, setUser] = useState({} as UserDTO) 
+  const [ isLoadingUserStorageData, setIsLoadingUserStorageData] = useState(true)
 
   async function signIn(email: string, password: string){
     try {
@@ -23,14 +26,37 @@ export function AuthContextProvider ({ children }: AuthContextProviderProps) {
 
       if(data.user){
         setUser(data.user)
+        storageUserSave(data.user)
       }
     } catch (error) {
       throw error
     }
   }
 
+  async function loadUserData() {
+    try {
+      const userLogged = await storageUserGet()
+
+      if(userLogged){
+        setUser(userLogged)
+        setIsLoadingUserStorageData(false)
+      }
+    } catch (error) {
+      throw error
+    }finally { 
+      setIsLoadingUserStorageData(false)
+    }
+  }
+
+  useEffect(() => {
+    loadUserData()
+  }, [])
   return (
-    <AuthContext.Provider value={{ user, signIn }}>
+    <AuthContext.Provider value={{ 
+      user, 
+      signIn,
+      isLoadingUserStorageData
+    }}>
 
       { children }
     </AuthContext.Provider>
