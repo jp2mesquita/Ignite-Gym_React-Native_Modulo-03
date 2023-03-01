94e83ce40b951218f6ce1428c7ebca6551cf1ae0
João Paulo Mesquita <jp-mesquita@live.com>
2023-03-01 12:10:22 -0300

    feat: create a new hook to deal with the context

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,17 +2,17 @@ import { useContext } from "react";
 import { NavigationContainer, DefaultTheme} from "@react-navigation/native";
 import { useTheme, Box } from "native-base";
 
-import { AuthContext } from "@contexts/AuthContext";
+import { useAuth } from "@hooks/useAuth";
 
 import { AuthRoutes } from "./auth.routes";
 import { AppRoutes } from "./app.routes";
 
 export function Routes(){
   const { colors } = useTheme()
+  const { user } = useAuth()
+  
 
-  const contextData = useContext(AuthContext)
-
-  console.log('USUÁRIO LOGADO', contextData )
+  console.log('USUÁRIO LOGADO', user )
 
   const theme = DefaultTheme
   theme.colors.background = colors.gray[700]
